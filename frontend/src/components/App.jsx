3658475a63b90d59a3ab4379a9c6c7da8d985ee4
anti1hero1 <nikitadanikita@gmail.com>
2023-09-06 18:32:21 +0300

    1

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,333 @@
+import React from "react";
+
+import { Route, Routes, useNavigate, Navigate } from "react-router-dom";
+
+import Header from "./Header";
+import Main from "./Main";
+import EditProfilePopup from "./EditProfilePopup";
+import EditAvatarPopup from "./EditAvatarPopup";
+import AddPlacePopup from "./AddPlacePopup";
+import PopupWithForm from "./PopupWithForm";
+import ImagePopup from "./ImagePopup";
+import Footer from "./Footer";
+
+import Login from "./Login";
+import Register from "./Register";
+import InfoTooltip from "./InfoTooltip";
+import ProtectedRoute from "./ProtectedRoute";
+
+import CurrentUserContext from "../contexts/CurrentUserContext";
+
+import api from "../utils/api";
+import * as apiAuth from "../utils/ApiAuth";
+
+function App() {
+  const [isEditAvatarPopupOpen, setAvatarPopupOpen] = React.useState(false);
+  const [isEditProfilePopupOpen, setProfilePopupOpen] = React.useState(false);
+  const [isAddPlacePopupOpen, setPlacePopupOpen] = React.useState(false);
+  const [isDeletePopupOpen, setDeletePopupOpen] = React.useState(false);
+  const [isImagePopup, setImagePopup] = React.useState(false);
+  const [isInfoTooltipPopup, setIsInfoTooltipPopup] = React.useState(false);
+
+  const [selectedCard, setSelectedCard] = React.useState({});
+  const [cards, setCards] = React.useState([]);
+  const [deleteCardId, setDeleteCardId] = React.useState("");
+
+  const [currentUser, setCurrentUser] = React.useState({});
+
+  const [loggedIn, setLoggedIn] = React.useState(false);
+  const [isInfoTooltipSuccess, setIsInfoTooltipSuccess] = React.useState(false);
+  // const [checkToken, setCheckToken] = React.useState(false);
+  const [headerEmail, setHeaderEmail] = React.useState("");
+
+  const [isLoading, setIsLoading] = React.useState(false);
+  const navigate = useNavigate();
+
+  // React.useEffect(() => {
+  //   const token = localStorage.getItem("jwt");
+  //   if (token) {
+  //     apiAuth
+  //       .checkToken(token)
+  //       .then((res) => {
+  //         if (res && res.data) {
+  //           setLoggedIn(true);
+  //           navigate("/");
+  //           setHeaderEmail(res.data.email);
+  //         }
+  //       })
+  //       .catch((err) => console.log(err));
+  //   } else {
+  //     setLoggedIn(false);
+  //   }
+  // }, []);
+
+  // React.useEffect(() => {
+  //   checkToken();
+  // }, [navigate]);
+
+  React.useEffect(() => {
+    checkToken();
+  }, []);
+
+  React.useEffect(() => {
+    // checkToken()
+    if (loggedIn) {
+      Promise.all([
+        api.getUserInfo(localStorage.jwt),
+        api.getCards(localStorage.jwt),
+      ])
+        .then(([user, cards]) => {
+          setCurrentUser({ ...currentUser, ...user });
+          setCards(cards);
+          // setCheckToken(true);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [loggedIn]);
+
+  function handleRegister(data) {
+    apiAuth
+      .register(data)
+      .then((res) => {
+        if (res && res.data) {
+          setIsInfoTooltipSuccess(true);
+          navigate("/sign-in");
+        }
+      })
+      .catch((err) => {
+        setIsInfoTooltipSuccess(false);
+        console.log(err);
+      })
+      .finally(() => setIsInfoTooltipPopup(true));
+  }
+
+  function checkToken() {
+    if (localStorage.jwt) {
+      apiAuth
+        .checkToken(localStorage.jwt)
+        .then((res) => {
+          if (res && res.data) {
+            setLoggedIn(true);
+            navigate("/");
+            setHeaderEmail(res.data.email);
+          }
+        })
+        .catch((err) => console.log(err));
+    } else {
+      setLoggedIn(false);
+    }
+  }
+
+  function handleLogin(data) {
+    apiAuth
+      .login(data)
+      .then((res) => {
+        if (res && res.token) {
+          localStorage.setItem("jwt", res.token);
+          navigate("/");
+          setHeaderEmail(data.email);
+          setLoggedIn(true);
+        }
+      })
+      .catch((err) => {
+        setIsInfoTooltipSuccess(false);
+        setIsInfoTooltipPopup(true);
+        console.log(err);
+      })
+  }
+
+  function logOut() {
+    setLoggedIn(false);
+    localStorage.removeItem("jwt");
+    setHeaderEmail("");
+  }
+
+  function handleEditAvatarClick() {
+    setAvatarPopupOpen(true);
+  }
+
+  function handleEditProfileClick() {
+    setProfilePopupOpen(true);
+  }
+
+  function handleAddPlaceClick() {
+    setPlacePopupOpen(true);
+  }
+
+  function handleDeleteClick(cardId) {
+    setDeleteCardId(cardId);
+    setDeletePopupOpen(true);
+  }
+
+  function handleCardClick(card) {
+    setSelectedCard(card);
+    setImagePopup(true);
+  }
+
+  function closeAllPopups() {
+    setAvatarPopupOpen(false);
+    setProfilePopupOpen(false);
+    setPlacePopupOpen(false);
+    setImagePopup(false);
+    setDeletePopupOpen(false);
+    setIsInfoTooltipPopup(false);
+  }
+
+  function handleDeleteSubmit(evt) {
+    evt.preventDefault();
+    api
+      .deleteCard(deleteCardId, localStorage.jwt)
+      .then(() => {
+        setCards((state) =>
+          state.filter((card) => {
+            return card._id !== deleteCardId;
+          })
+        );
+        closeAllPopups();
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function handleUpdateUser(user, reset) {
+    api
+      .changeUserInfo(user, localStorage.jwt)
+      .then((res) => {
+        setCurrentUser(res);
+        closeAllPopups();
+        reset();
+      })
+      .catch((err) => console.log(err))
+      .finally(() => setIsLoading(false));
+    setIsLoading(true);
+  }
+
+  function handleUpdateAvatar(user, reset) {
+    api
+      .changeAvatar(user, localStorage.jwt)
+      .then((res) => {
+        setCurrentUser(res);
+        closeAllPopups();
+        reset();
+      })
+      .catch((err) => console.log(err))
+      .finally(() => setIsLoading(false));
+    setIsLoading(true);
+  }
+
+  function handleAddPlaceSubmit(card, reset) {
+    api
+      .addNewCard(card, localStorage.jwt)
+      .then((newCard) => {
+        setCards([newCard, ...cards]);
+        closeAllPopups();
+        reset();
+      })
+      .catch((err) => console.log(err))
+      .finally(() => setIsLoading(false));
+    setIsLoading(true);
+  }
+
+  const handleLike = React.useCallback(
+    (card) => {
+      const isLike = card.likes.some(
+        (element) => currentUser._id === element
+      );
+      if (isLike) {
+        api
+          .deleteCardLike(card._id, localStorage.jwt)
+          .then((res) => {
+            setCards((state) =>
+              state.map((c) => (c._id === card._id ? res : c))
+            );
+          })
+          .catch((err) => console.log(err));
+      } else {
+        api
+          .getCardLike(card._id, localStorage.jwt)
+          .then((res) => {
+            setCards((state) =>
+              state.map((c) => (c._id === card._id ? res : c))
+            );
+          })
+          .catch((err) => console.log(err));
+      }
+    },
+    [currentUser._id]
+  );
+
+  return (
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="page">
+        <Header loggedIn={loggedIn} email={headerEmail} logOut={logOut} />
+
+        <Routes>
+          <Route
+            path="/sign-up"
+            element={<Register onRegister={handleRegister} />}
+          />
+          <Route path="/sign-in" element={<Login onLogin={handleLogin} />} />
+          <Route
+            path="*"
+            element={<Navigate to={loggedIn ? "/" : "/sign-in"} />}
+          />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute
+                // checkToken={checkToken}
+                loggedIn={loggedIn}
+                element={Main}
+                onEditAvatar={handleEditAvatarClick}
+                onEditProfile={handleEditProfileClick}
+                onAddPlace={handleAddPlaceClick}
+                onCardClick={handleCardClick}
+                onDelete={handleDeleteClick}
+                cards={cards}
+                onCardLike={handleLike}
+              />
+            }
+          />
+        </Routes>
+        <EditProfilePopup
+          onUpdateUser={handleUpdateUser}
+          isOpen={isEditProfilePopupOpen}
+          onClose={closeAllPopups}
+          isLoading={isLoading}
+        />
+        <EditAvatarPopup
+          onUpdateAvatar={handleUpdateAvatar}
+          onClose={closeAllPopups}
+          isOpen={isEditAvatarPopupOpen}
+          isLoading={isLoading}
+        />
+        <AddPlacePopup
+          onClose={closeAllPopups}
+          isOpen={isAddPlacePopupOpen}
+          onAddPlace={handleAddPlaceSubmit}
+          isLoading={isLoading}
+        />
+        <PopupWithForm
+          name="delete"
+          title="Вы уверены?"
+          buttonText={"Да"}
+          isOpen={isDeletePopupOpen}
+          onClose={closeAllPopups}
+          onSubmit={handleDeleteSubmit}></PopupWithForm>
+        <ImagePopup
+          card={selectedCard}
+          isOpen={isImagePopup}
+          onClose={closeAllPopups}
+        />
+        <InfoTooltip
+          name="tooltip"
+          isOpen={isInfoTooltipPopup}
+          onClose={closeAllPopups}
+          isSuccess={isInfoTooltipSuccess}
+        />
+        {loggedIn && <Footer />}
+      </div>
+    </CurrentUserContext.Provider>
+  );
+}
+
+export default App;
---
+++ frontend/src/utils/api.js
@@ -0,0 +1,106 @@
+
+class Api {
+  constructor(config) {
+    this._url = config.url;
+  }
+
+  _checkResponse(res) { return res.ok ? res.json() : Promise.reject }
+
+  getUserInfo(token) {
+    return fetch(`${this._url}/users/me`, {
+      headers: {
+        "Authorization": `Bearer ${token}`
+      }
+    })
+      .then(this._checkResponse)
+  }
+
+  getCards(token) {
+    return fetch(`${this._url}/cards`, {
+      headers: {
+        "Authorization": `Bearer ${token}`
+      }
+    })
+      .then(this._checkResponse)
+  }
+
+  addNewCard(data, token) {
+    return fetch(`${this._url}/cards`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        "Authorization": `Bearer ${token}`
+      },
+      body: JSON.stringify({
+        name: data.title,
+        link: data.link
+      })
+    })
+      .then(this._checkResponse)
+  }
+
+  deleteCard(cardId, token) {
+    return fetch(`${this._url}/cards/${cardId}`, {
+      method: "DELETE",
+      headers: {
+        "Authorization": `Bearer ${token}`
+      }
+    })
+      .then(this._checkResponse)
+  }
+
+  getCardLike(cardId, token) {
+    return fetch(`${this._url}/cards/${cardId}/likes`, {
+      method: "PUT",
+      headers: {
+        "Authorization": `Bearer ${token}`
+      }
+    })
+      .then(this._checkResponse)
+  }
+
+  deleteCardLike(cardId, token) {
+    return fetch(`${this._url}/cards/${cardId}/likes`, {
+      method: "DELETE",
+      headers: {
+        "Authorization": `Bearer ${token}`
+      }
+    })
+      .then(this._checkResponse)
+  }
+
+  changeAvatar(data, token) {
+    return fetch(`${this._url}/users/me/avatar`, {
+      method: "PATCH",
+      headers: {
+        'Content-Type': 'application/json',
+        "Authorization": `Bearer ${token}`
+      },
+      body: JSON.stringify({
+        avatar: data.avatar
+      })
+    })
+      .then(this._checkResponse)
+  }
+
+  changeUserInfo(data, token) {
+    return fetch(`${this._url}/users/me`, {
+      method: "PATCH",
+      headers: {
+        'Content-Type': 'application/json',
+        "Authorization": `Bearer ${token}`
+      },
+      body: JSON.stringify({
+        name: data.name,
+        about: data.about
+      }),
+    })
+      .then(this._checkResponse)
+  }
+}
+
+  const api = new Api({
+    url: 'http://localhost:3000',
+  });
+
+export default api
